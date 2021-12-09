b50981a5080cfe4906e2fdbafeb853ce1c649475
v-zhajin <v-zhajin@06W11zhajin000>
2021-12-09 00:35:03 -0800

    This is the second update

---
+++ WebApplication3/Test.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace WebApplication3
+{
+    public class Test
+    {
+        public void Test1()
+        {
+            Console.WriteLine("This is the second update");
+        }
+    }
+}
